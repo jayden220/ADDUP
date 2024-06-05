88880cc50c696be25344c382693a0e4821e71247
jayden220 <jaydenvanrooyen87@gmail.com>
2024-06-05 09:26:21 +0200

    final changes

--- script/script.js
+++ script/script.js
@@ -2,10 +2,19 @@
 let answer = document.getElementById('answer');
 let inputArray = document.getElementById('inputArray');
 let calculateButton = document.getElementById('calculateButton');
+let clearButton = document.getElementById('resetButton');
 
 calculateButton.addEventListener('click', function() {
     let inputValues = inputArray.value.split(',').map(Number); 
     let sum = inputValues.reduce((total, currentValue) => total + currentValue, 0);
     answer.innerHTML = `${sum} is the sum of the array`;
 });
+
+
+function reset() {
+    answer.innerHTML = ''
+    inputArray.value = ''
+}
+
+clearButton.addEventListener('click', reset)
 
